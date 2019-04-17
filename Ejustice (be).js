9a38b3d6b76eb47417039d43aa4a42246fee4c64
Mathijs van Westendorp <mvwestendorp@gmail.com>
2019-04-17 13:34:01 +0200

    Use production version for frames.

--- Ejustice (be).js
+++ Ejustice (be).js
@@ -1,22 +1,22 @@
 {
-	"translatorID": "c0e84400-1ed4-4cfa-9f88-a8e60e37a0cf",
-	"label": "Ejustice (be)",
+	"translatorID": "caf063b0-050d-4d10-ad05-11d406fd000c",
+	"label": "ejustice",
 	"creator": "Mathijs van Westendorp",
-	"target": "www.ejustice.just.fgov.be\\/(cgi_loi|eli\\/wet)",
+	"target": "www.ejustice.just.fgov.be.(cgi_loi|eli)",
 	"minVersion": "3.0",
 	"maxVersion": "",
 	"priority": 100,
 	"inRepository": true,
 	"translatorType": 4,
 	"browserSupport": "gcsibv",
-	"lastUpdated": "2019-03-14 17:21:35"
+	"lastUpdated": "2019-04-17 11:32:41"
 }
 
 function attr(docOrElem,selector,attr,index){var elem=index?docOrElem.querySelectorAll(selector).item(index):docOrElem.querySelector(selector);return elem?elem.getAttribute(attr):null}function text(docOrElem,selector,index){var elem=index?docOrElem.querySelectorAll(selector).item(index):docOrElem.querySelector(selector);return elem?elem.textContent:null}
 
 function detectWeb(doc, url) {
 	// Adjust the inspection of url as required
-	if ( url.indexOf('wet') != -1 ) {
+	if ( url.indexOf('wet') != -1 || url.indexOf('loi') != -1 ) {
 		return 'statute';
 	}
 	// Adjust the inspection of url as required
@@ -40,7 +40,7 @@ function doWeb(doc, url) {
 function getSearchResults(doc, checkOnly) {
 	var items = {};
 	var found = false;
-	// Adjust the CSS Selectors 
+	// Adjust the CSS Selectors
 	var rows = doc.querySelectorAll('.detailsLink');
 	for (var i=0; i<rows.length; i++) {
 		// Adjust if required, use Zotero.debug(rows) to check
@@ -55,17 +55,21 @@ function getSearchResults(doc, checkOnly) {
 	return found ? items : false;
 }
 
+
+
 function scrape(doc, url) {
 	item = new Zotero.Item(detectWeb(doc, url));
-
-	var rows;
-	if (url.indexOf('eli/wet') != -1 ){
-		rows = doc.querySelectorAll('h3 center u');
+	const frame = doc.querySelector('frameset');
+	Z.debug(frame);
+	if (frame){
+		Z.debug('Need Frame!');
+		doc = frame;
 	}
-	else { // cgi_loi
-		//rows = doc.querySelectorAll('b')
-		rows = frames[0].document.getElementsByTagName('b'); //production
-	}
+	Z.debug("Document" + JSON.stringify(doc));
+	//const rows = doc.querySelectorAll('tr th b');
+	// production code
+	const rows = frames[0].document.getElementsByTagName('b');
+	Z.debug("Rows length: "+ rows.length + "Rows: " + rows);
 	item = getLawEnacted(rows);
 	item.jurisdiction = 'be';
 	item.complete();
@@ -73,56 +77,24 @@ function scrape(doc, url) {
 
 function getLawEnacted(lineList){
 	item = new Zotero.Item('statute');
+	//Z.debug("Linelist length" + lineList.length);
 	for (i=0; i < lineList.length; i++){
-			var m = lineList[i].textContent.match(/(\d{1,2}\s[A-Z]*?\s\d{4})\.\s-\s([^<]*)/);
-			if (m){
-				item.dateEnacted = m[1].toLowerCase();
-				Z.debug("Date Enacted: " + item.dateEnacted);
-				item.nameOfAct = ZU.trimInternal(m[2]);
-				item.nameOfAct = item.nameOfAct.replace(/[\[\]]/g, "");
-				item.nameOfAct = item.nameOfAct.replace(/\.$/, "");
+		Z.debug("Line text" + lineList[i].textContent);
+		var m = lineList[i].textContent.match(/(\d{1,2}\s[A-Z]*?\s\d{4})\.\s-\s([^<^;^\.^\(]*)/);
+		if (m){
+			item.dateEnacted = m[1].toLowerCase();
+			Z.debug("Date Enacted: " + item.dateEnacted);
+			item.nameOfAct = ZU.trimInternal(m[2]);
+			item.nameOfAct = item.nameOfAct.replace(/[\[\]]/g, "");
+			item.nameOfAct = item.nameOfAct.replace(/\.$/, "");
+
+			if(item.nameOfAct.match(/Wet|Koninklijk besluit/)){
 				item.nameOfAct = item.nameOfAct.replace(/(^(Wet|Koninklijk besluit)*\b)/, "$1 van " + item.dateEnacted.toLowerCase())
-				Z.debug("Title: " + item.nameOfAct);
-				break;
 			}
+			Z.debug("Title: " + item.nameOfAct);
+			Z.debug("Item: " + item);
+			break;
 		}
+	}
 	return item
 }
-/** BEGIN TEST CASES **/
-var testCases = [
-	{
-		"type": "web",
-		"url": "http://www.ejustice.just.fgov.be/cgi_loi/loi_a1.pl?language=nl&la=N&cn=2002082245&table_name=wet&&caller=list&N&fromtab=wet&tri=dd+AS+RANK&rech=1&numero=1&sql=(text+contains+(%27%27))",
-		"items": [
-			{
-				"itemType": "statute",
-				"nameOfAct": "Wet van 22 augustus 2002 betreffende de rechten van de patiënt",
-				"creators": [],
-				"dateEnacted": "22 augustus 2002",
-				"jurisdiction": "be",
-				"attachments": [],
-				"tags": [],
-				"notes": [],
-				"seeAlso": []
-			}
-		]
-	},
-	{
-		"type": "web",
-		"url": "http://www.ejustice.just.fgov.be/cgi_loi/loi_a1.pl?language=nl&la=N&table_name=wet&cn=1967111008&&caller=list&N&fromtab=wet&tri=dd+AS+RANK&rech=1&numero=1&sql=(text+contains+(%27%27))",
-		"items": [
-			{
-				"itemType": "statute",
-				"nameOfAct": "Koninklijk besluit van 10 november 1967 nr 78 betreffende de uitoefening van de gezondheidszorgberoepen",
-				"creators": [],
-				"dateEnacted": "10 november 1967",
-				"jurisdiction": "be",
-				"attachments": [],
-				"tags": [],
-				"notes": [],
-				"seeAlso": []
-			}
-		]
-	}
-]
-/** END TEST CASES **/
