5e1b371e54f6629aeac2f6dce43c891470277699
Mathijs van Westendorp <mvwestendorp@gmail.com>
2019-04-17 12:12:59 +0200

    Fixed error in framehandling of ejustice translator

--- Ejustice (be).js
+++ Ejustice (be).js
@@ -2,14 +2,14 @@
 	"translatorID": "c0e84400-1ed4-4cfa-9f88-a8e60e37a0cf",
 	"label": "Ejustice (be)",
 	"creator": "Mathijs van Westendorp",
-	"target": "www.ejustice.just.fgov.be\\/(cgi_loi|eli\\/wet)",
+	"target": "www.ejustice.just.fgov.be\\/(cgi_loi|eli\\/(wet|loi))",
 	"minVersion": "3.0",
 	"maxVersion": "",
 	"priority": 100,
 	"inRepository": true,
 	"translatorType": 4,
 	"browserSupport": "gcsibv",
-	"lastUpdated": "2019-03-14 17:21:35"
+	"lastUpdated": "2019-04-17 10:06:45"
 }
 
 function attr(docOrElem,selector,attr,index){var elem=index?docOrElem.querySelectorAll(selector).item(index):docOrElem.querySelector(selector);return elem?elem.getAttribute(attr):null}function text(docOrElem,selector,index){var elem=index?docOrElem.querySelectorAll(selector).item(index):docOrElem.querySelector(selector);return elem?elem.textContent:null}
@@ -57,15 +57,15 @@ function getSearchResults(doc, checkOnly) {
 
 function scrape(doc, url) {
 	item = new Zotero.Item(detectWeb(doc, url));
-
+	Z.debug("Document" + doc);
 	var rows;
-	if (url.indexOf('eli/wet') != -1 ){
-		rows = doc.querySelectorAll('h3 center u');
+	const frame = doc.querySelector('frame');
+	Z.debug("Document frame" + frame);
+	if (frame){
+		doc = frame;
 	}
-	else { // cgi_loi
-		//rows = doc.querySelectorAll('b')
-		rows = frames[0].document.getElementsByTagName('b'); //production
-	}
+	rows = doc.querySelectorAll('tr th');
+	Z.debug("Rows" + rows);
 	item = getLawEnacted(rows);
 	item.jurisdiction = 'be';
 	item.complete();
@@ -73,15 +73,20 @@ function scrape(doc, url) {
 
 function getLawEnacted(lineList){
 	item = new Zotero.Item('statute');
+	//Z.debug("Linelist length" + lineList.length);
 	for (i=0; i < lineList.length; i++){
-			var m = lineList[i].textContent.match(/(\d{1,2}\s[A-Z]*?\s\d{4})\.\s-\s([^<]*)/);
+		Z.debug("Line text" + lineList[i].textContent);
+			var m = lineList[i].textContent.match(/(\d{1,2}\s[A-Z]*?\s\d{4})\.\s-\s([^<^;^\.^\(]*)/);
 			if (m){
 				item.dateEnacted = m[1].toLowerCase();
 				Z.debug("Date Enacted: " + item.dateEnacted);
 				item.nameOfAct = ZU.trimInternal(m[2]);
 				item.nameOfAct = item.nameOfAct.replace(/[\[\]]/g, "");
 				item.nameOfAct = item.nameOfAct.replace(/\.$/, "");
-				item.nameOfAct = item.nameOfAct.replace(/(^(Wet|Koninklijk besluit)*\b)/, "$1 van " + item.dateEnacted.toLowerCase())
+		
+				if(item.nameOfAct.match(/Wet|Koninklijk besluit/)){
+					item.nameOfAct = item.nameOfAct.replace(/(^(Wet|Koninklijk besluit)*\b)/, "$1 van " + item.dateEnacted.toLowerCase())
+				}
 				Z.debug("Title: " + item.nameOfAct);
 				break;
 			}
@@ -116,6 +121,41 @@ var testCases = [
 				"nameOfAct": "Koninklijk besluit van 10 november 1967 nr 78 betreffende de uitoefening van de gezondheidszorgberoepen",
 				"creators": [],
 				"dateEnacted": "10 november 1967",
+				"jurisdiction": "be",
+				"attachments": [],
+				"tags": [],
+				"notes": [],
+				"seeAlso": []
+			}
+		]
+	},
+	{
+		"type": "web",
+		"url": "http://www.ejustice.just.fgov.be/cgi_loi/loi_a1.pl?language=nl&la=N&cn=1967101004&table_name=wet&&caller=list&N&fromtab=wet&tri=dd+AS+RANK&rech=1&numero=1&sql=(text+contains+(%27%27))",
+		"items": [
+			{
+				"itemType": "statute",
+				"nameOfAct": "GERECHTELIJK WETBOEK - Deel IV : BURGERLIJKE RECHTSPLEGING",
+				"creators": [],
+				"dateEnacted": "10 oktober 1967",
+				"jurisdiction": "be",
+				"shortTitle": "GERECHTELIJK WETBOEK - Deel IV",
+				"attachments": [],
+				"tags": [],
+				"notes": [],
+				"seeAlso": []
+			}
+		]
+	},
+	{
+		"type": "web",
+		"url": "http://www.ejustice.just.fgov.be/cgi_loi/loi_a1.pl?language=nl&la=N&cn=2008122334&table_name=wet&&caller=list&N&fromtab=wet&tri=dd+AS+RANK&rech=1&numero=1&sql=(text+contains+(%27%27))",
+		"items": [
+			{
+				"itemType": "statute",
+				"nameOfAct": "Koninklijk besluit van 23 december 2008 tot uitvoering van de arbeidsongevallenwet van 10 april 1971 in verband met de onevenredig verzwaarde risico's",
+				"creators": [],
+				"dateEnacted": "23 december 2008",
 				"jurisdiction": "be",
 				"attachments": [],
 				"tags": [],
